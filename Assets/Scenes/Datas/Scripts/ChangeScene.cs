1e23303e7fb6136e21514ba472eaf679ff3a412e
moha003p <48089659+moha003p@users.noreply.github.com>
2020-07-13 22:36:11 +0200

    initial commit

---
+++ Assets/Scenes/Datas/Scripts/ChangeScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScene : MonoBehaviour
+{
+
+    public void ChangeLaScene(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
+
+}
---
+++ Assets/Scenes/Datas/Scripts/SkyBox2.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyBox2 : MonoBehaviour {
+
+    public Material skyOne;
+    public Material skyTwo;
+    public Material skyThree;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        RenderSettings.skybox = skyTwo;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
